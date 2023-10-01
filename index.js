af2c3f5d7cda1f4aa7c9986a435dabf2c399f8dd
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-01 21:18:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = cats.push("Ralph");
+
+const destructivelyPrependCat = cats.unshift("Bob");
+
+const destructivelyRemoveLastCat = cats.pop();
+
+const destructivelyRemoveFirstCat = cats.shift();
+
+const appendCat = [...cats,"Broom "];
+
+
+const removeLastCat = cats.slice(0, cats.length-1);
+
+const removeFirstCat = cats.slice(1);
